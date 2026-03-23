b1785f0b87a53ccec4959a16acb90be9f8d23484
Aravind <gmarav005@gmail.com>
2026-03-23 17:30:44 +0530

    Add constant for branding document ID
    chore(#10539): add BRANDING constant and migrate hardcoded string usages
    
    Signed-off-by: Aravind <gmarav005@gmail.com>

--- admin/src/js/filters/resource-icon.js
+++ admin/src/js/filters/resource-icon.js
@@ -21,7 +21,7 @@ angular.module('inboxFilters').filter('headerLogo',
     'use strict';
     'ngInject';
     return function(name) {
-      return $sce.trustAsHtml(ResourceIcons.getImg(name, 'branding'));
+      return $sce.trustAsHtml(ResourceIcons.getImg(name, DOC_IDS.BRANDING));
     };
   });
 
@@ -36,3 +36,4 @@ angular.module('inboxFilters').filter('partnerImage',
       return $sce.trustAsHtml(ResourceIcons.getImg(name, DOC_IDS.PARTNERS));
     };
   });
+  
